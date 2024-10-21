8d412814ae254eb66c4f61f40a7cf651d571599a
Ivy Li <107764260+xiangivyli@users.noreply.github.com>
2024-10-21 11:05:45 +0100

    Create two_integer_sum.py

---
+++ Leetcode_Questions/arrays_hashing/two_integer_sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {} # initiate an empty dic to store value:index
+        for i, value in enumerate(nums):
+            complement = target - value
+            if complement in hashmap:
+                return [hashmap[complement], i]
+            else:
+                hashmap[value] = i
+
+# hashmap and enumerate
+# Time complexity O(n) and Space complexity O(n)
+# The algorithm iterates through the list of numbers (nums) exactly once in a single loop. Each lookup and insertion into the hashmap takes constant time 
+𝑂(1) on average.
+# The space complexity comes from the hashmap, which stores each number from nums along with its index.
